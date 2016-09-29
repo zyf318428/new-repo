c584f0093d510afc0488e6d0cb21d69dabf405db
zyf318428 <1113618708@qq.com>
2016-09-29 16:44:54 +0800

    lendbike
    lendbike

--- lendbike.go
+++ lendbike.go
@@ -72,7 +72,19 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 	fmt.Println("query is running " + function)
 
 	// Handle different functions
-	if function == "read" { //read a variable
+	if function == "readPerson" { //read a variable
+		return t.read(stub, args)
+	}
+	// Handle different functions
+	if function == "readTime" { //read a variable
+		return t.read(stub, args)
+	}
+	// Handle different functions
+	if function == "readPosition" { //read a variable
+		return t.read(stub, args)
+	}
+	// Handle different functions
+	if function == "readState" { //read a variable
 		return t.read(stub, args)
 	}
 	fmt.Println("query did not find func: " + function)
@@ -90,21 +102,21 @@ func (t *SimpleChaincode) write(stub *shim.ChaincodeStub, args []string) ([]byte
 		return nil, errors.New("Incorrect number of arguments. Expecting 4. name of the key and value to set")
 	}
 
-	err = stub.PutState("person", []byte(args[0]))
+	_, err = stub.PutState("person", []byte(args[0]))
 	if err != nil {
 		return nil, err
 	}
 
-	err = stub.PutState("time", []byte(args[1]))
+	_, err = stub.PutState("time", []byte(args[1]))
 	if err != nil {
 		return nil, err
 	}
 
-	err = stub.PutState("position", []byte(args[2]))
+	_, err = stub.PutState("position", []byte(args[2]))
 	if err != nil {
 		return nil, err
 	}
-	err = stub.PutState("state", []byte(args[3]))
+	_, err = stub.PutState("state", []byte(args[3]))
 	if err != nil {
 		return nil, err
 	}
@@ -113,42 +125,77 @@ func (t *SimpleChaincode) write(stub *shim.ChaincodeStub, args []string) ([]byte
 }
 
 // read - query function to read key/value pair
-func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]string, error) {
+func (t *SimpleChaincode) readPerson(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	var key, jsonResp string
-	var result []string
 	var err error
 
-	if len(args) != 0 {
-		return nil, errors.New("Incorrect number of arguments. ")
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 	}
 
-	person, err := stub.GetState(person)
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
 
-	time, err := stub.GetState(time)
+	return valAsbytes, nil
+}
+
+// read - query function to read key/value pair
+func (t *SimpleChaincode) readTime(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
 
-	position, err := stub.GetState(position)
+	return valAsbytes, nil
+}
+
+// read - query function to read key/value pair
+func (t *SimpleChaincode) readPosition(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
 
-	state, err := stub.GetState(state)
+	return valAsbytes, nil
+}
+
+// read - query function to read key/value pair
+func (t *SimpleChaincode) readstate(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
 
-	result[0] = person
-	result[1] = time
-	result[2] = position
-	result[3] = state
-	return result, nil
+	return valAsbytes, nil
 }
